57a1adde1220a8993c8f3fce72b7796eb9b565d0
Austin Pejovich <pejaustin@gmail.com>
2018-09-16 18:45:11 -0400

    added node movement

--- grid_movement/grid/grid.gd
+++ grid_movement/grid/grid.gd
@@ -14,20 +14,22 @@ func get_cell_pawn(cell, type = ACTOR):
 		if world_to_map(node.position) == cell:
 			return(node)
 
+func try_direction(cell_start, direction):
+	var cell_target = cell_start + direction
+	var cell_target_type = get_cellv(cell_target)
+	while cell_target_type != OBJECT and cell_target_type != ACTOR:
+		cell_target = cell_target + direction
+		cell_target_type = get_cellv(cell_target)
+		if cell_target.x <0 or cell_target.x > 1300 or cell_target.y <0 or cell_target.y > 1300:
+			return cell_start
+	return cell_target
+			
 func request_move(pawn, direction):
 	var cell_start = world_to_map(pawn.position)
-	var cell_target = cell_start + direction
-	
+	var cell_target = try_direction(cell_start, direction)
 	var cell_tile_id = get_cellv(cell_target)
-	match cell_tile_id:
-		-1:
-			set_cellv(cell_target, ACTOR)
-			set_cellv(cell_start, -1)
-			return map_to_world(cell_target) + cell_size / 2
-		OBJECT, ACTOR:
-			var target_pawn = get_cell_pawn(cell_target, cell_tile_id)
-			print("Cell %s contains %s" % [cell_target, target_pawn.name])
-			
-			if not target_pawn.has_node("DialogPlayer"):
-				return
-			get_node(dialog_ui).show_dialog(pawn, target_pawn.get_node("DialogPlayer"))
+	var target_pawn = get_cell_pawn(cell_target,cell_tile_id)
+	if not target_pawn.has_node("DialogPlayer"):
+		return map_to_world(cell_target) + cell_size / 2
+	get_node(dialog_ui).show_dialog(pawn, target_pawn.get_node("DialogPlayer"))
+	return map_to_world(cell_target) + cell_size / 2
