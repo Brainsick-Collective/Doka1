15a068f1b1a42fa0a4396f63b9f1431e6ba64d33
Austin Pejovich <pejaustin@gmail.com>
2018-09-19 20:01:03 -0400

    added node movement

--- grid_movement/grid/grid.gd
+++ grid_movement/grid/grid.gd
@@ -5,7 +5,8 @@ export(NodePath) var dialog_ui
 
 func _ready():
 	for child in get_children():
-		set_cellv(world_to_map(child.position), child.type)
+		if child.type == OBJECT:
+			set_cellv(world_to_map(child.position), child.type)
 
 func get_cell_pawn(cell, type = ACTOR):
 	for node in get_children():
@@ -22,14 +23,20 @@ func try_direction(cell_start, direction):
 		cell_target_type = get_cellv(cell_target)
 		if cell_target.x <0 or cell_target.x > 1300 or cell_target.y <0 or cell_target.y > 1300:
 			return cell_start
+		print(cell_target)
 	return cell_target
 			
+func react_to_move(cell_target):
+	var target_space = get_cell_pawn(cell_target, OBJECT)
+	
 func request_move(pawn, direction):
 	var cell_start = world_to_map(pawn.position)
 	var cell_target = try_direction(cell_start, direction)
 	var cell_tile_id = get_cellv(cell_target)
-	var target_pawn = get_cell_pawn(cell_target,cell_tile_id)
-	if not target_pawn.has_node("DialogPlayer"):
-		return map_to_world(cell_target) + cell_size / 2
-	get_node(dialog_ui).show_dialog(pawn, target_pawn.get_node("DialogPlayer"))
+	var player_if_present = get_cell_pawn(cell_target,ACTOR)
+	if player_if_present:
+		get_node(dialog_ui).show_dialog(pawn, player_if_present.get_node("DialogPlayer"))
+	else:
+		var target_pawn = get_cell_pawn(cell_target, cell_tile_id)
+		get_node(dialog_ui).show_dialog(pawn, target_pawn.get_node("DialogPlayer"))
 	return map_to_world(cell_target) + cell_size / 2
--- grid_movement/pawns/pawn.gd
+++ grid_movement/pawns/pawn.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 enum CELL_TYPES { ACTOR, OBSTACLE, OBJECT }
-export(CELL_TYPES) var type = ACTOR
+export(CELL_TYPES) var type = OBJECT
 
 export (PackedScene) var combat_actor
 var active = true setget set_active
